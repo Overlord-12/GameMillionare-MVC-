b179406d876f974e0ac4a777a5cded304f46a666
@d.argastcev <danya.argastesv.02@mail.ru>
2021-09-08 19:10:29 +0300

    Исправление реализации сервиса и репозитория

---
+++ GameMillionare(MVC)/Models/Repository/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameMillionare_MVC_.Models.Repository
+{
+    interface IRepository<Answer> : IDisposable
+    {
+       List<Answer> GetAnswers(int val);
+    }
+}
---
+++ GameMillionare(MVC)/Models/Repository/SqlRepository.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace GameMillionare_MVC_.Models.Repository
+{
+    public class SqlRepository : IRepository<Answer>
+    {
+        static QuestionContext qc;
+        public SqlRepository()
+        {
+            qc = new QuestionContext();
+        }
+        private bool disposed = false;
+        public virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    qc.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        public List<Answer> GetAnswers(int val)
+        {
+            return qc.Answers.Include(p => p.Question).Take(val * 4).ToList();
+        }   
+    }
+}
